5a4ef8c7a9c8cd461491dd72d0478c6c4e3a3ebb
Hazlan Muhammad Qodri <byze.eternalground@gmail.com>
2023-08-14 11:57:54 +0700

    change latest to image_id and do terraform init and apply

--- docker.tf
+++ docker.tf
@@ -17,12 +17,12 @@ resource "docker_image" "nginx" {
 }
 
 resource "docker_container" "nginx" {
-  image = docker_image.nginx.latest
+  image = docker_image.nginx.image_id
   name = "tutorial"
 
   ports {
     internal = 80
     external = 8000
   }
-  
+
 }
