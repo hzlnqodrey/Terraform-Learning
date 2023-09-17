efe1b9098e509f99c0f935c2054f7974ebcf95be
Hazlan Muhammad Qodri <byze.eternalground@gmail.com>
2023-09-17 19:55:16 +0700

    add data source of network and create sub network 02

---
+++ terraform-om_giri_kuncoro/main.tf
@@ -0,0 +1,27 @@
+provider "google" {
+    project = "belajar-terraform-398813"
+    region = "asia-southeast2"
+}
+
+resource "google_compute_network" "development_network" {
+    name = "development-network"
+    auto_create_subnetworks = false
+}
+
+resource "google_compute_subnetwork" "dev-subnet-01" {
+    name = "dev-subnet-01"
+    ip_cidr_range = "10.100.0.0/16"
+    network = google_compute_network.development_network.id
+    region = "asia-southeast2"
+}
+
+data "google_compute_network" "existing_network" {
+    name = "default"
+}
+
+resource "google_compute_subnetwork" "dev-subnet-02" {
+    name = "dev-subnet-02"
+    ip_cidr_range = "10.110.0.0/16"
+    network = data.google_compute_network.existing_network.id
+    region = "asia-southeast2"
+}
