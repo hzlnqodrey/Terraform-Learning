124d59f267ee7f85290c8d13697e8dec9769722d
Hazlan Muhammad Qodri <byze.eternalground@gmail.com>
2023-09-18 20:20:33 +0700

    variable default values

--- terraform-om_giri_kuncoro/main.tf
+++ terraform-om_giri_kuncoro/main.tf
@@ -5,26 +5,35 @@ provider "google" {
 
 variable "subnet_ip_cidr_range" {
     description = "subnet untuk dev environment"
+    default     = "10.150.0.0/16" 
 }
 
 variable "subnet_secondary_ip_cidr_range" {
     description = "secondary ip range subnet untuk dev environment"
 }
 
+variable "network_name" {
+    description = "nama network kita"
+}
+
+variable "subnet_01_name" {
+    description = "nama subnet 01"
+}
+
 resource "google_compute_network" "development_network" {
-    name = "development-network"
+    name = var.network_name
     auto_create_subnetworks = false
 }
 
 resource "google_compute_subnetwork" "dev-subnet-01" {
-    name = "dev-subnet-01"
+    name = var.subnet_01_name
     ip_cidr_range = var.subnet_ip_cidr_range
     network = google_compute_network.development_network.id
     region = "asia-southeast2"
-    secondary_ip_range {
-        range_name = "secondary-range-01"
-        ip_cidr_range = var.subnet_secondary_ip_cidr_range
-    }
+    # secondary_ip_range {
+    #     range_name = "secondary-range-01"
+    #     ip_cidr_range = var.subnet_secondary_ip_cidr_range
+    # }
 }
 
 # data "google_compute_network" "existing_network" {
@@ -38,10 +47,10 @@ resource "google_compute_subnetwork" "dev-subnet-01" {
 #     region = "asia-southeast2"
 # }
 
-output "development_network_id" {
-    value = google_compute_network.development_network.id
-}
+# output "development_network_id" {
+#     value = google_compute_network.development_network.id
+# }
 
-output "dev_subnet_01_gateway" {
-    value = google_compute_subnetwork.dev-subnet-01.gateway_address
-}
+# output "dev_subnet_01_gateway" {
+#     value = google_compute_subnetwork.dev-subnet-01.gateway_address
+# }
--- terraform-om_giri_kuncoro/terraform-dev.tfvars
+++ terraform-om_giri_kuncoro/terraform-dev.tfvars
@@ -1,5 +1,5 @@
 # BEST PRACTICES FOR TERRAFORM INPUT VARIABLE
 subnet_ip_cidr_range = "10.130.0.0/16"
 subnet_secondary_ip_cidr_range = "192.168.20.0/24"
-network_name = "development_network"
+network_name = "development-network"
 subnet_01_name = "dev-subnet-01"
